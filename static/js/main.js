60a2ae9d5eaac9332880161f779f9e85fba27f40
Jed Boyle <jedboyle01@gmail.com>
2020-07-05 12:38:32 +0100

    Now, you must create a .forEach loop that will pass the elements in the notes array through your event assignment function.

--- static/js/main.js
+++ static/js/main.js
@@ -26,10 +26,9 @@ let eventAssignment = function(note) {
     }
 }
 
-
-
 // Write a loop that runs the array elements through the function
 
+notes.forEach(eventAssignment);
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
